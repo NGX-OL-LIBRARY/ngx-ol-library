db16793ca465c43e59fbdfb46c7b2fa367a3dc84
Jonnytoshen <jonnytoshen@gmail.com>
2024-02-26 14:19:45 +0800

    feat(module:layer-core): support overview map control for layer host

--- src/layer/core/layer-host.ts
+++ src/layer/core/layer-host.ts
@@ -1,4 +1,5 @@
 import { InjectOptions } from '@angular/core';
+import { useOverviewMapControl } from 'ngx-ol-library/control/overview-map';
 import { injectLayerGroup } from 'ngx-ol-library/layer/group';
 import { injectMap } from 'ngx-ol-library/map';
 import BaseLayer from 'ol/layer/Base';
@@ -6,6 +7,7 @@ import BaseLayer from 'ol/layer/Base';
 export function injectLayerHost(selector: string) {
   const options: InjectOptions = { optional: true, host: true };
   const map = injectMap(options);
+  const overviewMap = useOverviewMapControl(options);
   const layerGroup = injectLayerGroup(options);
 
   let getInstance, addLayer, removeLayer;
@@ -20,6 +22,14 @@ export function injectLayerHost(selector: string) {
       const layers = layerGroup.getInstance().getLayers();
       return layers.remove(layer);
     };
+  } else if (overviewMap) {
+    getInstance = () => overviewMap.getInstance();
+    addLayer = (layer: BaseLayer) =>{
+      return overviewMap.getInstance().getOverviewMap().addLayer(layer);
+    }
+    removeLayer = (layer: BaseLayer) => {
+      return overviewMap.getInstance().getOverviewMap().removeLayer(layer);
+    }
   } else if (map) {
     getInstance = () => map.getInstance();
     addLayer = (layer: BaseLayer) => map.getInstance().addLayer(layer);
@@ -27,7 +37,7 @@ export function injectLayerHost(selector: string) {
   } else {
     throw new Error(
       '`' + selector + '` component must be nested within `nol-map`' +
-      ' or `nol-layer-group` component.'
+      ', `nol-overview-map-control` or `nol-layer-group` component.'
     );
   }
 
