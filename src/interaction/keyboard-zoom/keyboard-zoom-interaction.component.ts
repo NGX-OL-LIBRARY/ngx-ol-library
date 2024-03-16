468fc895005f3075bb0c2b46393ff78c55c5e85e
Jonnytoshen <jonnytoshen@gmail.com>
2024-03-16 16:44:15 +0800

    refactor(module:keyboard-zoom): set true as defaults for nolActive

--- src/interaction/keyboard-zoom/keyboard-zoom-interaction.component.ts
+++ src/interaction/keyboard-zoom/keyboard-zoom-interaction.component.ts
@@ -37,7 +37,7 @@ import { injectMap } from 'ngx-ol-library/map';
 export class NolKeyboardZoomInteractionComponent
   implements NolPrefixedOptions<Options>, OnInit, OnChanges, OnDestroy {
 
-  @Input() nolActive?: boolean;
+  @Input() nolActive: boolean = true;
   @Input() nolDuration?: number;
   @Input() nolCondition?: Condition;
   @Input() nolDelta?: number;
@@ -64,9 +64,7 @@ export class NolKeyboardZoomInteractionComponent
       delta: this.nolDelta,
     });
 
-    if (typeof this.nolActive === 'boolean') {
-      this.instance.setActive(this.nolActive);
-    }
+    this.instance.setActive(this.nolActive);
 
     if (this.nolProperties) {
       this.instance.setProperties(this.nolProperties);
