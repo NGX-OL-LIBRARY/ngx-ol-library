eafea5065eb823168c41e00c937b4f9410972b80
Jonnytoshen <jonnytoshen@gmail.com>
2024-07-22 16:01:59 +0800

    feat(module:stroke-style): stroke for circle style

--- src/style/stroke/utils.ts
+++ src/style/stroke/utils.ts
@@ -1,16 +1,19 @@
 import { InjectOptions } from '@angular/core';
+import { useCircleStyle } from 'ngx-ol-library/style/circle';
 import { useRegularShapeStyle } from 'ngx-ol-library/style/regular-shape';
 import { useStyle } from 'ngx-ol-library/style/style';
 import { Stroke } from 'ol/style';
 
 export function useStrokeStyleHost() {
   const injectOptions: InjectOptions = { host: true, optional: true };
-  const host = useRegularShapeStyle() ||
+  const host = useCircleStyle() ||
+    useRegularShapeStyle() ||
     useStyle(injectOptions);
 
   if (!host) {
     throw new Error(
-      '`nol-stroke-style` component must be nested within `nol-style`, `nol-regular-shape-style` component.'
+      '`nol-stroke-style` component must be nested within `nol-style`, ' +
+      '`nol-regular-shape-style` or `nol-circle-style` component.'
     );
   }
 
