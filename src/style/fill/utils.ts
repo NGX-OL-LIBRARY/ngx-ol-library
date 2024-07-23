e7ed1673cdb7b07ea827a11b7399f3428cabeb2d
Jonnytoshen <jonnytoshen@gmail.com>
2024-07-23 08:11:37 +0800

    feat(module:fill-style): fill and background fill for text style

--- src/style/fill/utils.ts
+++ src/style/fill/utils.ts
@@ -2,12 +2,15 @@ import { InjectOptions } from '@angular/core';
 import { useCircleStyle } from 'ngx-ol-library/style/circle';
 import { useRegularShapeStyle } from 'ngx-ol-library/style/regular-shape';
 import { useStyle } from 'ngx-ol-library/style/style';
+import { useTextBackgroundFill, useTextStyle } from 'ngx-ol-library/style/text';
 import { Fill } from 'ol/style';
 
 export function useFillStyleHost() {
   const injectOptions: InjectOptions = { host: true, optional: true };
-  const host = useCircleStyle() ||
-    useRegularShapeStyle() ||
+  const host = useTextBackgroundFill(injectOptions) || 
+    useTextStyle(injectOptions) ||
+    useCircleStyle(injectOptions) ||
+    useRegularShapeStyle(injectOptions) ||
     useStyle(injectOptions);
 
   if (!host) {
